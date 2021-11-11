b90f72c4083efa5473213f19c36ce1d567702dd3
furious-monkey <daniel.j.hahn1105@gmail.com>
2021-11-11 15:46:20 +0100

    fix(recipes): print recipes even if local storage is empty
    When local storage was empty, the recipes was saved successfully but
    the recipes list was not displayed on screen without refreshing the
    browser.
    So I moved the logic to save the recipes from AppComponent to
    RecipesListComponent to avoid some repeat and I display recipes from
    API if local storage was empty before.

--- recipes/src/app/app.component.ts
+++ recipes/src/app/app.component.ts
@@ -1,35 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipes } from './shared/recipes';
-import { LocalStorageService } from './shared/services/local-storage.service';
-import { RecipesService } from './shared/services/recipes.service';
-import { slugify } from './shared/utilities/slugify';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Food Recipes';
 
-  constructor(
-    private recipesService: RecipesService,
-    private localStorage: LocalStorageService
-  ) {}
-
-  ngOnInit(): void {
-    if (!this.localStorage.get('recipes')) {
-      this.saveRecipes();
-    }
-  }
-
-  saveRecipes(): void {
-    this.recipesService.getAllRecipes().subscribe((recipes: any) => {
-      const recipesArray = recipes.meals;
-      const newRecipesArray = recipesArray.map((recipe: Recipes) => {
-        return { ...recipe, slug: slugify(recipe.strMeal) };
-      });
-      this.localStorage.set('recipes', newRecipesArray);
-    });
-  }
+  constructor() {}
 }
--- recipes/src/app/components/recipes-list/recipes-list.component.ts
+++ recipes/src/app/components/recipes-list/recipes-list.component.ts
@@ -1,21 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Recipes } from 'src/app/shared/recipes';
 import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
+import { RecipesService } from 'src/app/shared/services/recipes.service';
+import { slugify } from 'src/app/shared/utilities/slugify';
 
 @Component({
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss'],
 })
 export class RecipesListComponent implements OnInit {
-  recipes: Recipes[] = [];
+  private _recipes = new BehaviorSubject<Recipes[]>([]);
+  public recipes = this._recipes.asObservable();
 
-  constructor(private storage: LocalStorageService) {}
+  constructor(
+    private storage: LocalStorageService,
+    private recipesService: RecipesService
+  ) {}
 
   ngOnInit(): void {
-    this.getRecipes();
+    if (this.storage.get('recipes')) {
+      this._recipes.next(this.storage.get('recipes'));
+    } else {
+      this.setRecipes();
+      this.saveRecipes();
+    }
   }
 
-  getRecipes(): void {
-    this.recipes = this.storage.get('recipes');
+  setRecipes(): any {
+    this.recipesService.getAllRecipes().subscribe((recipes: any) => {
+      const allRecipes: Recipes[] = recipes.meals;
+      const allRecipesWithSlug = allRecipes.map((recipe: Recipes) => {
+        return { ...recipe, slug: slugify(recipe.strMeal) };
+      });
+      this._recipes.next(allRecipesWithSlug);
+    });
+  }
+
+  saveRecipes(): void {
+    this.recipes.subscribe((recipes) => this.storage.set('recipes', recipes));
   }
 }
